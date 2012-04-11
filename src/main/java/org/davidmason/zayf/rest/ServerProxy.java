02b164e981e0172d611898084d235b784482d4d4
David Mason <dr.d.mason@gmail.com>
2012-04-11 11:53:49 +1000

    add rest communication methods and stub implementations

--- src/main/java/org/davidmason/zayf/rest/ServerProxy.java
+++ src/main/java/org/davidmason/zayf/rest/ServerProxy.java
@@ -18,23 +18,25 @@
  */
 package org.davidmason.zayf.rest;
 
-//standard libs
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
-//zanata-rest-client
-import org.zanata.rest.client.ZanataProxyFactory;
-
-//zanata-common-api
-import org.zanata.rest.dto.VersionInfo;
-import org.zanata.rest.dto.Project;
-import org.zanata.rest.client.IProjectResource;
-import org.zanata.rest.client.ITranslationResources;
-import org.zanata.rest.dto.resource.ResourceMeta;
-
-//resteasy-jaxrs
 import org.jboss.resteasy.client.ClientResponse;
+import org.zanata.common.EntityStatus;
+import org.zanata.common.LocaleId;
+import org.zanata.rest.client.ITranslationResources;
+import org.zanata.rest.client.ZanataProxyFactory;
+import org.zanata.rest.dto.Project;
+import org.zanata.rest.dto.VersionInfo;
+import org.zanata.rest.dto.resource.Resource;
+import org.zanata.rest.dto.resource.ResourceMeta;
+import org.zanata.rest.dto.resource.TextFlow;
+import org.zanata.rest.dto.resource.TextFlowTarget;
+import org.zanata.rest.dto.resource.TranslationsResource;
 
 /**
  * Provides a proxy to a Zanata server
@@ -42,9 +44,10 @@ import org.jboss.resteasy.client.ClientResponse;
 public class ServerProxy
 {
 
+   private static final String SERVER_VERSION = "1.5.0";
+   private static final String SERVER_BUILD_TIMESTAMP = "unknown";
+
    private URI serverURI;
-   private String userName;
-   private String apiKey;
 
    private ZanataProxyFactory requestFactory;
 
@@ -61,17 +64,89 @@ public class ServerProxy
    public ServerProxy(URI serverURI, String userName, String apiKey)
    {
       this.serverURI = serverURI;
-      this.userName = userName;
-      this.apiKey = apiKey;
 
-      requestFactory = new ZanataProxyFactory(serverURI, userName, apiKey,
-                                              new VersionInfo("1.5.0", "unknown"));
+      requestFactory =
+            new ZanataProxyFactory(serverURI, userName, apiKey,
+                                   new VersionInfo(SERVER_VERSION, SERVER_BUILD_TIMESTAMP));
       /*
               new ZanataProxyFactory(new URL("http://localhost:8080/zanata/").toURI(), 
                                      "admin",
                                      "REDACTED", 
                                      new VersionInfo("1.5.0", "unknown"));
       */
+   }
+
+   // TODO add constructor with flag to make a pure dummy version, or make a
+   // subclass with dummy implementation
+
+   /**
+    * Get a list of projects on the server.
+    * 
+    * FIXME Currently returns dummy data
+    * 
+    * @return a list of all active and read-only projects on the server
+    */
+   public List<Project> getProjectList()
+   {
+      /* FIXME this should work, but gives 404, may need to update library code
+      URI projectsUri = getProjectsURI(serverURI);
+      System.out.println("Projects URI: " + projectsUri);
+      IProjectsResource projectListResource = requestFactory.getProjects(serverURI);
+
+      ClientResponse<Project[]> projectListResponse = projectListResource.get();
+      if (projectListResponse.getStatus() >= 399)
+      {
+         //TODO throw specific useful exception
+          System.out.println("Got error response code: " + projectListResponse.getStatus());
+          throw new RuntimeException("Got error response code retrieving project list: " + projectListResponse.getStatus());
+      }
+      else
+      {
+          return Arrays.asList(projectListResponse.getEntity()); //new ArrayList<Project>
+      }
+      */
+
+      ArrayList<Project> projects = new ArrayList<Project>();
+
+      Project proj1 = new Project();
+      proj1.setId("dummy1");
+      proj1.setName("Dummy Project 1");
+      proj1.setDescription("A dummy project created for testing");
+      proj1.setStatus(EntityStatus.ACTIVE);
+      projects.add(proj1);
+
+      Project proj2 = new Project();
+      proj2.setId("dummy2");
+      proj2.setName("Dummy Project 2");
+      proj2.setDescription("Another dummy project created for testing");
+      proj2.setStatus(EntityStatus.ACTIVE);
+      projects.add(proj2);
+
+      Project proj3 = new Project();
+      proj3.setId("dummy3");
+      proj3.setName("Dummy Project 3");
+      proj3.setDescription("A read-only dummy project created for testing");
+      proj3.setStatus(EntityStatus.READONLY);
+      projects.add(proj3);
+      return projects;
+   }
+
+   /**
+    * Get a list of versions for a project
+    * 
+    * @param projectSlug
+    *           identifying slug for the project
+    * @return list of version slugs
+    */
+   public List<String> getVersionList(String projectSlug)
+   {
+      // FIXME looks like our rest interface doesn't have any way to discover this
+      // information. Will need to update the interface to provide it
+      List<String> versions = new ArrayList<String>();
+      versions.add(projectSlug + "-v1");
+      versions.add(projectSlug + "-v2");
+      versions.add(projectSlug + "-v3");
+      return versions;
    }
 
    /**
@@ -98,4 +173,51 @@ public class ServerProxy
       return getResponse.getEntity();
    }
 
+   public List<TextFlow> getTextFlows(String projectSlug, String versionSlug, String docId)
+   {
+      ITranslationResources translationResources =
+            requestFactory.getTranslationResources(projectSlug, versionSlug);
+      ClientResponse<Resource> response =
+            translationResources.getResource(prepareDocId(docId), null);
+      Resource res = response.getEntity();
+      return res.getTextFlows();
+   }
+
+   public List<TextFlowTarget> getTargets(String projectSlug, String versionSlug, LocaleId locale,
+                                          String docId)
+   {
+      ITranslationResources translationResources =
+            requestFactory.getTranslationResources(projectSlug, versionSlug);
+      ClientResponse<TranslationsResource> response =
+            translationResources.getTranslations(prepareDocId(docId), locale, null);
+      TranslationsResource res = response.getEntity();
+      return res.getTextFlowTargets();
+   }
+
+   //TODO get list of supported locales for server, project, version
+
+   private static String RESOURCE_PREFIX = "rest";
+
+   //TODO move this method into ZanataProxyFactory
+   public static URI getProjectsURI(URI baseURI)
+   {
+      String spec = RESOURCE_PREFIX + "/projects";
+      try
+      {
+         return new URL(baseURI.toURL(), spec).toURI();
+      }
+      catch (MalformedURLException e)
+      {
+         throw new RuntimeException(e);
+      }
+      catch (URISyntaxException e)
+      {
+         throw new RuntimeException(e);
+      }
+   }
+
+   private String prepareDocId(String docId)
+   {
+      return docId.replace('/', ',');
+   }
 }
